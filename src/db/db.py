4e478ae486b068f7083d31cf1baf10dd04b27b4e
pitpek <d.federyakin@inbox.ru>
2023-09-05 01:51:01 +0300

    скрыты чувствительные данные бд

--- src/db/db.py
+++ src/db/db.py
@@ -3,15 +3,6 @@ from sqlalchemy.orm import DeclarativeBase
 
 from configs.config import settings
 
-# DB_HOST = "localhost"
-# DB_PORT = 5432
-# DB_USER = "postgres"
-# DB_PASS = "postgres"
-# DB_NAME = "postgres"
-#
-# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-
 DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL)
